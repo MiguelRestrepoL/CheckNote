95c279feb13e8557653251c86eec054167ebcbca
DAMN05 <168138215+DAMN05@users.noreply.github.com>
2025-09-14 22:52:47 -0500

    .

--- Backend/TO-DO-LIST-SERVER/api/routes/UserRoutes.js
+++ Backend/TO-DO-LIST-SERVER/api/routes/UserRoutes.js
@@ -6,11 +6,11 @@ const {authenticateToken} = require('../middleware/authMiddleware');
 
 const userController = UserController;
 
-router.get("/", (req, res) => userController.getAll(req, res));
-router.get("/:id", (req, res) => userController.read(req, res));
-router.post("/", (req, res) => userController.create(req, res));
-router.put("/:id", (req, res) => userController.update(req, res));
-router.delete("/:id", (req, res) => userController.delete(req, res));
+//router.get("/", (req, res) => userController.getAll(req, res));
+//router.get("/:id", (req, res) => userController.read(req, res));
+//router.post("/", (req, res) => userController.create(req, res));
+//router.put("/:id", (req, res) => userController.update(req, res));
+//router.delete("/:id", (req, res) => userController.delete(req, res));
 
 /**
  * GET /api/v1/users/me
