14fb3760cef44aa0414f76a8119ad3b3fb9cfd19
DAMN05 <168138215+DAMN05@users.noreply.github.com>
2025-09-14 22:45:30 -0500

    .

--- Backend/TO-DO-LIST-SERVER/api/routes/UserRoutes.js
+++ Backend/TO-DO-LIST-SERVER/api/routes/UserRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require("../controllers/UserController");
-const {authenticateToken} = require('../middlewares/authMiddleware');
+const {authenticateToken} = require('../middleware/authMiddleware');
 
 
 const userController = new UserController();
