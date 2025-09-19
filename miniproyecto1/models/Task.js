8290b0823b96440478b05f0030c8c211c90851b6
juanflorezdiaz-creator <juan.florez.diaz@correounivalle.edu.co>
2025-09-19 14:15:50 -0500

    Update Task.js

--- miniproyecto1/models/Task.js
+++ miniproyecto1/models/Task.js
@@ -2,15 +2,16 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-  titulo:    { type: String, required: true },
-  detalles:   { type: String },
-  descripcion:   { type: String },
-  prioridad:   { type: String },
-  fechaVencimiento: { type: Date },
-  estado:   { type: String, enum: ["pendiente", "completa"], default: "pendiente" },
-  clienteId:   { type: mongoose.Schema.Types.ObjectId, ref: "cliente" }
+  title:    { type: String, required: true },
+  details:   { type: String },
+  description:   { type: String },
+  priority:   { type: String },
+  expirationdate: { type: Date },
+  state:   { type: String, enum: ["pendiente", "completa"], default: "pendiente" },
+  Id:   { type: mongoose.Schema.Types.ObjectId, ref: "cliente" }
 }, { timestamps: true });
 
 module.exports = mongoose.model("Tarea", TaskSchema);
+
 
 
