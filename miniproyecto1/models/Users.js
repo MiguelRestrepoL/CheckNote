55f1fec0e87d971015d62859545579ffce8c4f19
juanflorezdiaz-creator <juan.florez.diaz@correounivalle.edu.co>
2025-09-19 14:11:45 -0500

    Update Users.js

--- miniproyecto1/models/Users.js
+++ miniproyecto1/models/Users.js
@@ -1,16 +1,17 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  apellido: { type: String, required: true },
-  edad: { type: Number },
-  correo:    { type: String, required: true, unique: true },
-  contraseña: { type: String, required: true },
+  name: { type: String, required: true },
+  lastname: { type: String, required: true },
+  age: { type: Number },
+  email:    { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   resetToken:    { type: String },
   resetTokenExp: { type: Date }
 }, { timestamps: true });
 
 module.exports = mongoose.model("Usuario ", UserSchema)
+
 
 
 
