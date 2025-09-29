c2fc839eb34d2f1bc632519a70bcda6d6ac82125
MiguelRestrepoL <ith1zdragonz@gmail.com>
2025-09-28 20:04:09 -0500

    Frontend - Fully functional, React JSX -> Pure HTML JS.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html'),
+        registro: resolve(__dirname, 'registro.html'),
+        'olvidar-password1': resolve(__dirname, 'olvidar-password1.html'),
+        home: resolve(__dirname, 'home.html'),
+        perfil: resolve(__dirname, 'perfil.html'),
+        'olvidar-password2': resolve(__dirname, 'olvidar-password2.html'),
+        'crear-tarea': resolve(__dirname, 'crear-tarea.html'),
+        'editar-tarea': resolve(__dirname, 'editar-tarea.html'),
+        'eliminar-tarea': resolve(__dirname, 'eliminar-tarea.html'),
+        'eliminar-cuenta': resolve(__dirname, 'eliminar-cuenta.html'),
+        'sobre-nosotros': resolve(__dirname, 'sobre-nosotros.html')
+      }
+    }
+  }
 });
